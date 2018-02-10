639e8fee27606068357d483663b2dca4c21e3b7e
Joseph Sikorski <joedski@gmail.com>
2018-02-10 13:11:41 -0500

    INit commit.

---
+++ index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const Koa = require('koa');
+const KoaStatic = require('koa-static');
+const WebSocket = require('ws');
+
+const child_process = require('child_process');
+
+const WS_PORT = process.env.WS_PORT || 9090;
+const wss = new WebSocket.Server({ port: WS_PORT });
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log('received: %s', message);
+
+    child_process.exec(message, (error, stdout, stderr) => {
+      console.log('Executed command!');
+      if (error) {
+        ws.send(JSON.stringify({ error }));
+      }
+      else {
+        ws.send(JSON.stringify({ stdout, stderr }));
+      }
+    });
+  });
+
+  // ws.send('something');
+});
+
+console.log(`websocket server listening on ${WS_PORT}`);
+
+const HTTP_PORT = process.env.HTTP_PORT || 8080;
+
+const app = new Koa();
+
+app.use(KoaStatic(path.resolve(__dirname, 'static')));
+
+app.listen(HTTP_PORT);
+
+console.log(`http server listening on ${HTTP_PORT}`);
